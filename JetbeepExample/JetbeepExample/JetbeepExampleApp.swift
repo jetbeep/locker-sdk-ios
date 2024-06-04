77a7ac9b88ab20ddc8b4354e2c2ac16b8d9bf2a4
Max Tymchii <samback@ukr.net>
2024-06-04 10:59:32 +0100

    Add example project.

---
+++ JetbeepExample/JetbeepExample/JetbeepExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetbeepExampleApp.swift
+//  JetbeepExample
+//
+//  Created by Max Tymchii on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct JetbeepExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
